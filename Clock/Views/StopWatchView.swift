44aac71a8f7d3dbe7e97a2d4400057178f901e89
Ray Huang <rhuang@lcs.on.ca>
2026-02-23 14:44:01 -0500

    Clock 2

---
+++ Clock/Views/StopWatchView.swift
@@ -0,0 +1,18 @@
+//
+//  StopWatchView.swift
+//  Clock
+//
+//  Created by Panyui Huang on 2026-02-23.
+//
+
+import SwiftUI
+
+struct StopWatchView: View {
+    var body: some View {
+        Text("Stop Watch")
+    }
+}
+
+#Preview {
+    StopWatchView()
+}
---
+++ Clock/Views/WorldClockView.swift
@@ -0,0 +1,18 @@
+//
+//  WorldClockView.swift
+//  Clock
+//
+//  Created by Panyui Huang on 2026-02-23.
+//
+
+import SwiftUI
+
+struct WorldClockView: View {
+    var body: some View {
+        Text("World Clock")
+    }
+}
+
+#Preview {
+    WorldClockView()
+}
