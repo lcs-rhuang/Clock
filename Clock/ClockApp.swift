dff7570613a2ee7feaa8061d42e49cd7f798e872
Ray Huang <rhuang@lcs.on.ca>
2026-02-23 14:26:35 -0500

    Initial Commit

---
+++ Clock/ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockApp.swift
+//  Clock
+//
+//  Created by Panyui Huang on 2026-02-23.
+//
+
+import SwiftUI
+
+@main
+struct ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
